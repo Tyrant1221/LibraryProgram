1d4357821aa4c184dd2b6a826d43c0b03103064f
Tyrant1221 <165565603+Tyrant1221@users.noreply.github.com>
2026-01-11 12:03:43 -0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,143 @@
+public class Book { 
+
+    // Member variables (private) 
+
+    private int id; 
+
+    private String title; 
+
+    private String author; 
+
+    private String isbn; 
+
+    private int pages; 
+
+  
+
+    // Default constructor 
+
+    public Book() { 
+
+    } 
+
+  
+
+    // Parameterized constructor 
+
+    public Book(int id, String title, String author, String isbn, int pages) { 
+
+        this.id = id; 
+
+        this.title = title; 
+
+        this.author = author; 
+
+        this.isbn = isbn; 
+
+        this.pages = pages; 
+
+    } 
+
+  
+
+    // Getters and setters 
+
+    public int getId() { 
+
+        return id; 
+
+    } 
+
+  
+
+    public void setId(int id) { 
+
+        this.id = id; 
+
+    } 
+
+  
+
+    public String getTitle() { 
+
+        return title; 
+
+    } 
+
+  
+
+    public void setTitle(String title) { 
+
+        this.title = title; 
+
+    } 
+
+  
+
+    public String getAuthor() { 
+
+        return author; 
+
+    } 
+
+  
+
+    public void setAuthor(String author) { 
+
+        this.author = author; 
+
+    } 
+
+  
+
+    public String getIsbn() { 
+
+        return isbn; 
+
+    } 
+
+  
+
+    public void setIsbn(String isbn) { 
+
+        this.isbn = isbn; 
+
+    } 
+
+  
+
+    public int getPages() { 
+
+        return pages; 
+
+    } 
+
+  
+
+    public void setPages(int pages) { 
+
+        this.pages = pages; 
+
+    } 
+
+  
+
+    // Print book information 
+
+    public void printBookInfo() { 
+
+        System.out.println("ID: " + id); 
+
+        System.out.println("Title: " + title); 
+
+        System.out.println("Author: " + author); 
+
+        System.out.println("ISBN: " + isbn); 
+
+        System.out.println("Pages: " + pages); 
+
+        System.out.println("----------------------"); 
+
+    } 
+
+} 
